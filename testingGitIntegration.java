952ee06f45bd75e6387fcbbfe74ccc65f21ac029
Vignesh S <80673377+Vignesh-tech360@users.noreply.github.com>
2023-03-19 00:07:04 +0530

    Update testingGitIntegration.java

--- testingGitIntegration.java
+++ testingGitIntegration.java
@@ -1,6 +1,6 @@
 public class main{
 public static void main(String[] args){
   System.out.println("hi hello");
-  system.out.println("intergraion working pproperly");
+  System.out.println("intergraion working pproperly");
 }
 }
